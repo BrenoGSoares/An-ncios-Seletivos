d3b7ebdc2a9a39c39b3cab59cd0b3034cc7d1737
Breno <breno.givas@icloud.com>
2023-11-27 18:12:08 -0300

    feat: info by use of prefix

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
+from collections import Counter
+import json
+
 data_file = r"saida_rib.20181201.0800.txt"
 processed_data = []
-asn = []
 
 
 try:
@@ -27,38 +29,71 @@ try:
             for element in reversed(line[1]):
                 if owner_asn != element:
                     line[1].index(element)
-                    print(line[1][line[1].index(element)])
                     return [line[0], owner_asn, line[1][line[1].index(element)]]
         return [line[0], owner_asn, line[1][-2]]
 
     def neighbors(data):
         neighbors_dict = {}
         for route in data:
-            owner = "Owner: " + route[1]
             prefix = route[0]
+            owner = route[1]
             value = route[2]
 
             if owner not in neighbors_dict:
-                neighbors_dict[owner] = []
+                neighbors_dict[owner] = {}
 
-            found = False
-            for entry in neighbors_dict[owner]:
-                if entry[0] == prefix:
-                    entry[1].append(value)
-                    found = True
-                    break
+            if prefix not in neighbors_dict[owner]:
+                neighbors_dict[owner][prefix] = []
 
-            if not found:
-                neighbors_dict[owner].append([prefix, [value]])
+            neighbors_dict[owner][prefix].append(value)
 
-        neighbors_list = [[owner, entries] for owner, entries in neighbors_dict.items()]
-        return neighbors_list
+        return neighbors_dict
+
+    def show_info(data):
+        for owner, inner_dict in data.items():
+            neighbors = len([asn for values in inner_dict.values() for asn in values])
+            unique_neighbors = len(
+                set.union(*[set(values) for values in inner_dict.values()])
+            )
+            print(
+                f"Owner: {owner} | Neighbors {neighbors} | Unique Neighbors: {unique_neighbors}"
+            )
+
+            # asn_list = []
+            # repeated_asn = []
+            # for prefix, values in inner_dict.items():
+            #     for element in values:
+            #         if element in asn_list:
+            #             repeated_asn.append(element)
+            #         asn_list.append(element)
+
+            for prefix, values in inner_dict.items():
+                seen_asns = set()
+                repeated_asn_prefix = []
+
+                # Verificar ASN repetidos
+                for asn in values:
+                    if asn in seen_asns:
+                        repeated_asn_prefix.append(asn)
+
+                    else:
+                        seen_asns.add(asn)
+
+                unique_percentage = (
+                    (len(values) - len(repeated_asn_prefix)) / unique_neighbors
+                ) * 100
+
+                print(
+                    f"  Prefixo: {prefix} - {len(values)} ASN Use: {unique_percentage:.2f}% |"
+                )
 
     def main():
+        asn = []
         for route in processed_data:
             asn.append(check_valid_asn(route))
         valid_asn = list(filter(None, asn))
-        print(neighbors(valid_asn))
+        show_info(neighbors(valid_asn))
+        # print(json.dumps(neighbors(valid_asn), indent=2))
         pass
 
     main()
